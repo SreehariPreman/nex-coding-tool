4506d281497c0876707c0d7e85ec5966732a7f63
SreehariPreman <sreeharipreman853@gmail.com>
2026-04-03 18:58:57 +0530

    nex: @print_primes_till_20.py refactor the code print till 10 and

--- print_primes_till_10.py
+++ print_primes_till_10.py
@@ -1,12 +1,17 @@
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
 
-print('Prime numbers till 10:')
+print("Prime numbers till 10:")
 for num in range(2, 11):
     if is_prime(num):
-        print(num)
+        print(num, end=" ")
+print()
