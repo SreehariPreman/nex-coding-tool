9601d6c8ed99e3d99ae57788d3429643cf0895b1
SreehariPreman <sreeharipreman853@gmail.com>
2026-04-03 18:35:39 +0530

    nex: a python file to print prime numbers till 20

---
+++ print_primes_till_20.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+print("Prime numbers till 20:")
+for num in range(2, 21):
+    if is_prime(num):
+        print(num, end=" ")
+print()
